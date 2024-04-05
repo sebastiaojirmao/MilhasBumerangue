e608b5463eca809f4b4c7536d28b0b95da6668a1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 20:34:49 -0300

    Resolve o exercicio MilhasBumerangue

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double percentualDeBonus, percentualDeRetorno;
+double pontosATranferir, pontosAReduzir, pontosAAcrecentar;
+
+Console.Clear();
+Console.WriteLine("---Milhas Bumerangue ---\n");
+
+Console.Write("Percentual de bônus.....:");
+percentualDeBonus = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Percentual de retorno.....:");
+percentualDeRetorno = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Pontos a transferir.....:");
+pontosATranferir = Convert.ToDouble(Console.ReadLine());
+
+pontosAReduzir = pontosATranferir - (pontosATranferir * percentualDeRetorno) / 100;
+pontosAAcrecentar = pontosATranferir + (pontosATranferir * percentualDeBonus) / 100;
+
+Console.WriteLine();
+Console.WriteLine ($"Pontos a reduzir na origem.........: {pontosAReduzir}");
+Console.WriteLine ($"Pontos a acrescentar no destino....: {pontosAAcrecentar}");
